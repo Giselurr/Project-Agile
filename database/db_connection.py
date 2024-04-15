83172bb973445afba72f61ae93e95d585cbe0c06
Pernilla Wennerberg <pernilla.ahlstrom_wennerberg0018@stud.hkr>
2024-04-15 15:49:14 +0200

    GROUP-7 added connection to the database

---
+++ database/db_connection.py
@@ -0,0 +1,16 @@
+""" This class creates the connection to the database """
+import mysql.connector
+import mysql.connector.cursor
+
+class db_connection:
+    """ Creates the basic database connection add your own information. """
+    def __init__(self, user, password):
+        """ Initialize the db_connection class with the connection login information """
+        self.con = {"user": user, "password": password, "host": "localhost", "port": 3306, 
+        "database": "breathe", "raise_on_warnings": True}
+
+    def connect(self):
+        """ Makes the connection to the database and return the database curser """
+        self.my_con = mysql.connector.connect(**self.con)
+        self.curser = mysql.connector.cursor(self.my_con)
+        return self.curser
