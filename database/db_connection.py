a3fd932c77821102ae79d1c6a72031659f715e5f
Giselurr <gisela.malmberg0007@stud.hkr.se>
2024-04-18 11:49:16 +0200

    ran pre commit on all files

--- database/db_connection.py
+++ database/db_connection.py
@@ -1,18 +1,27 @@
-""" This class creates the connection to the database """
+"""This class creates the connection to the database"""
+
 import mysql.connector
 import mysql.connector.cursor
 
-class db_connection:
-    """ Creates the basic database connection add your own information. """
+
+class DbConnection:
+    """Creates the basic database connection add your own information."""
+
     def __init__(self, user, password):
-        """ Initialize the db_connection class with the connection login information """
-        self.con = {"user": user, "password": password, "host": "localhost", "port": 3306, 
-        "database": "breathe", "raise_on_warnings": True}
+        """Initialize the db_connection class with the connection login information"""
+        self.con = {
+            "user": user,
+            "password": password,
+            "host": "localhost",
+            "port": 3306,
+            "database": "breathe",
+            "raise_on_warnings": True,
+        }
         self.my_con = None
         self.cursor = None
 
     def connect(self):
-        """ Makes the connection to the database and return the database curser """
+        """Makes the connection to the database and return the database curser"""
         self.my_con = mysql.connector.connect(**self.con)
         self.cursor = self.my_con.cursor()
         return self.cursor
