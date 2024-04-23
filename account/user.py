7c949dbe8aba14e3f1f05d09322d593ef6341bb3
Pernilla Ahlström Wennerberg <pernilla.ahlstrom-wennerberg0018@stud.hkr.se>
2024-04-23 08:53:43 +0200

    GROUP-26 added images to buttons updated the test file and added command to the buttons

--- account/user.py
+++ account/user.py
@@ -1,6 +1,6 @@
 """This module will store the user information and the GUI for the user page"""
 
-from tkinter import Button, Frame, Label
+from tkinter import Button, Frame, Label, PhotoImage, Tk
 
 
 class User:
@@ -18,6 +18,11 @@ class User:
         self.window.resizable(height=True, width=False)
         self.window.config(bg="#040B20")
         if self.is_logged_in:
+            breathe_img = PhotoImage(file="account\images/breathe.png")
+            scheduel_img = PhotoImage(file="account\images/scheduel.png")
+            stresslevel_img = PhotoImage(file="account\images/stresslevel.png")
+            calendar_img = PhotoImage(file="account\images/calendar.png")
+            history_img = PhotoImage(file="account\images/history.png")
             self.window.title(f"Welcome {self.user_name}!")
             user_frame = Frame(self.window)
             user_frame.configure(bg="#040B20")
@@ -26,52 +31,61 @@ class User:
                 user_frame,
                 text="Welcome " + self.user_name + ", what would you like to do? ",
                 bg="#040B20",
-                fg="#76ADCF",
+                fg="#AFB5D6",
                 font=("Arial", 18),
             ).grid(row=1, column=0, columnspan=4)
             Label(user_frame, bg="#040B20", text="").grid(row=2)
             Button(
                 user_frame,
-                text="BREATHE",
-                bg="#76ADCF",
-                fg="#040B20",
-                font=("Arial", 20, "bold"),
-                width="25",
-                height="2",
+                image=breathe_img,
+                borderwidth=0,
+                command=lambda: self.redirect("breathe", user_frame),
+                highlightthickness=0,
+                bd=0,
+                padx=0,
+                pady=0,
             ).grid(row=3, column=0, columnspan=4)
             Label(user_frame, bg="#040B20", text="").grid(row=4)
             Button(
                 user_frame,
-                text="Set schedule",
-                bg="#76ADCF",
-                fg="#040B20",
-                font=("Arial", 15, "bold"),
-                width="16",
+                image=scheduel_img,
+                borderwidth=0,
+                command=lambda: self.redirect("set schedule", user_frame),
+                highlightthickness=0,
+                bd=0,
+                padx=0,
+                pady=0,
             ).grid(row=5, column=0, columnspan=2)
             Button(
                 user_frame,
-                text="Set you stresslevel",
-                bg="#76ADCF",
-                fg="#040B20",
-                font=("Arial", 15, "bold"),
-                width="16",
+                image=stresslevel_img,
+                borderwidth=0,
+                command=lambda: self.redirect("set your stress level", user_frame),
+                highlightthickness=0,
+                bd=0,
+                padx=0,
+                pady=0,
             ).grid(row=5, column=2, columnspan=2)
             Label(user_frame, bg="#040B20", text="").grid(row=6)
             Button(
                 user_frame,
-                text="View your calender",
-                bg="#76ADCF",
-                fg="#040B20",
-                font=("Arial", 15, "bold"),
-                width="16",
+                image=calendar_img,
+                borderwidth=0,
+                command=lambda: self.redirect("view your calender", user_frame),
+                highlightthickness=0,
+                bd=0,
+                padx=0,
+                pady=0,
             ).grid(row=7, column=0, columnspan=2)
             Button(
                 user_frame,
-                text="Stress history",
-                bg="#76ADCF",
-                fg="#040B20",
-                font=("Arial", 15, "bold"),
-                width="16",
+                image=history_img,
+                borderwidth=0,
+                command=lambda: self.redirect("stress history", user_frame),
+                highlightthickness=0,
+                bd=0,
+                padx=0,
+                pady=0,
             ).grid(row=7, column=2, columnspan=2)
             user_frame.pack()
         else:
@@ -82,9 +96,29 @@ class User:
             Label(
                 user_frame,
                 text="Please go back and login to your accout ",
-                bg="#040B20",
-                fg="#76ADCF",
+                bg="#AFB5D6",
+                fg="#040B20",
                 font=("Arial", 18),
             ).grid(row=1, column=0, columnspan=4)
             user_frame.pack()
         self.window.mainloop()
+
+    def redirect(self, redirect_to, user_frame):
+        user_frame.pack_forget()
+        redirect = Frame(self.window)
+        redirect.configure(bg="#AFB5D6")
+        Label(redirect, bg="#AFB5D6", text="").grid(row=0)
+        Label(
+            redirect,
+            text="Redirecting to the " + redirect_to + ".",
+            bg="#AFB5D6",
+            fg="#040B20",
+            font=("Arial", 12),
+        ).grid(row=1, column=0)
+        redirect.pack()
+
+
+window = Tk()
+user = User(True, "Nilla", window)
+user.user_gui()
+"""bg 040B20 fg AFB5D6"""
--- test/test_user.py
+++ test/test_user.py
@@ -15,17 +15,20 @@ class TestUser(TestCase):
         self.mock_user_frame = MagicMock()
 
     def test_user_init(self):
+        """Test so that the class is being instanciated."""
         self.mock_logged_in = MagicMock
         self.user = user.User(self.mock_logged_in, self.mock_user, self.mock_window)
         self.assertIsInstance(self.user, user.User)
 
     def test_user_logged_in_true(self):
+        """Test if the user is logged in."""
         logged_in = True
         self.user = user.User(logged_in, self.mock_user, self.mock_window)
 
         self.assertTrue(logged_in)
 
     def test_user_logged_in_false(self):
+        """Test so that the user is not logged in."""
         logged_in = False
         self.user = user.User(logged_in, self.mock_user, self.mock_window)
 
